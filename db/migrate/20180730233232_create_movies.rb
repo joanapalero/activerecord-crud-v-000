6aca83eab8f566953bdc173c255a01de6bf00cb6
Joana Palero <joana.palero24@gmail.com>
2018-07-30 23:36:21 +0000

    automatically backed up by learn

--- db/migrate/20180730233232_create_movies.rb
+++ db/migrate/20180730233232_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :title, :director, :lead
-    add_column :release_date
-    add_column :in_theaters
+    add_column :title, :director, :lead, :string
+    add_column :release_date, :integer
+    add_column :in_theaters, :boolean
   end
 end
